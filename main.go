a82c7912fc1de4510ae3d82b916189dba5f0f310
Marko Mikulicic <mmikulicic@gmail.com>
2022-09-12 22:38:09 +0200

    Add binlog

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/alecthomas/kong"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/binarylog"
 )
 
 type Context struct {
@@ -13,7 +20,30 @@ type CLI struct {
 	Serve ServeCmd `cmd:"" help:"Serve"`
 }
 
+func setupBinaryLogging() {
+	if _, found := os.LookupEnv("GRPC_BINARY_LOG_FILTER"); !found {
+		return
+	}
+
+	grpc.EnableTracing = true
+	sink, err := binarylog.NewTempFileSink()
+	if err != nil {
+		log.Fatal(err)
+	}
+	binarylog.SetSink(sink)
+}
+
 func main() {
+	setupBinaryLogging()
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		binarylog.SetSink(nil)
+		os.Exit(0)
+	}()
+
 	var cli CLI
 	ctx := kong.Parse(&cli)
 	err := ctx.Run(&Context{CLI: &cli})
